525e69b9d2d268e68a914c7a0bf1b25667661a30
Stijn Callebaut <stijn.callebaut@outlook.be>
2021-03-25 11:17:13 +0100

    chore: py linting

--- app/src/hellothere/__init__.py
+++ app/src/hellothere/__init__.py
@@ -2,13 +2,12 @@ from flask import Flask
 from hellothere.config import Config
 
 
-
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
-    
-    from hellothere.main.routes import main   
-    from hellothere.errors.handlers import errors     
+    from hellothere.errors.handlers import errors
+    from hellothere.main.routes import main
+
     app.register_blueprint(main)
     app.register_blueprint(errors)
 
