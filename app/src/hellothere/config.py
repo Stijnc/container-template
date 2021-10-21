548690b84c00676678dbcb6438e7f6af66379e40
Stijn Callebaut <stijn.callebaut@outlook.be>
2021-10-21 21:07:04 +0200

    feat: adds subtitle

--- app/src/hellothere/config.py
+++ app/src/hellothere/config.py
@@ -9,6 +9,7 @@ class Config:
     BACKGROUND_COLOR = os.getenv("BACKGROUND_COLOR") or "#1d1e20"
     COLOR = os.getenv("COLOR") or "#fff"
     NAME = os.getenv("NAME") or "there"
+    TEXT = os.getenv("TEXT") or ""
     IMG_URI = (
         os.getenv("IMG_URI")
         or "https://raw.githubusercontent.com/Stijnc/Stijnc/main/img/profile_round.png"
--- app/src/hellothere/main/routes.py
+++ app/src/hellothere/main/routes.py
@@ -9,10 +9,12 @@ def home():
     background_color = current_app.config["BACKGROUND_COLOR"]
     color = current_app.config["COLOR"]
     name = current_app.config["NAME"]
+    text = current_app.config["TEXT"]
     img_uri = current_app.config["IMG_URI"]
     return render_template(
         "index.html",
         name=name,
+        text=text,
         background_color=background_color,
         color=color,
         img_uri=img_uri,
